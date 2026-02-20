d04e6b060ab900b8540f5d3d27ba715f07ad1d9b
fss1208 <fss1208@naver.com>
2026-02-20 14:56:40 +0900

    20260220e. Cross Origin 버그 수정

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ app = FastAPI(title="가보자GO", version="0.1.0", description="Backend API Spe
 # ]
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
