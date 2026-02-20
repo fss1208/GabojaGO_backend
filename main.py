ca6b11717e0bdc8ddfe04ff4b37cf92a09ebdce9
fss1208 <fss1208@naver.com>
2026-02-20 14:53:04 +0900

    20260220d. Cross Origin 설정

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import logging
 import os
@@ -17,6 +18,18 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 logger.debug("LOG_LEVEL 설정 ({})".format(logging.getLevelName(log_level)))
 app = FastAPI(title="가보자GO", version="0.1.0", description="Backend API Specification")
+
+# 1. 허용할 출처(Origin) 목록 정의
+# origins = [
+#     "http://localhost:3000",      # React/Next.js 기본 포트
+#     "https://example.com"         # 실제 운영 도메인
+# ]
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # prefix: 모든 경로 앞에 '/auth'가 자동으로 붙음 (예: /auth/login)
 # tags: '/docs' & '/redoc' 페이지에서 해당 그룹으로 묶어서 표시
