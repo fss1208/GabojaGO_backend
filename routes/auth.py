f5b5962a320b7480dda4eaba9d0aa7dd02e1faee
fss1208 <fss1208@naver.com>
2026-02-21 17:01:54 +0900

    20260221f. 로그 정리

--- routes/auth.py
+++ routes/auth.py
@@ -16,40 +16,53 @@ logger = logging.getLogger(__name__)
 
 @router.post("/register", summary="회원가입", response_model=UserModel)
 def register(user_model: UserModel):
-    logger.debug(f"Request Body {user_model.model_dump()}")
+    """
+    회원가입
+    - **id**: 필수 입력
+    - **pw**: 필수 입력
+    - **name**: 필수 입력
+    - **email**: 필수 입력
+    """    
+    logger.info(f"회원가입 요청 ({user_model})")
     with DB.CONNECT() as connection:
         with connection.cursor() as cursor:
             result = DB.EXECUTE(cursor, user_model.select_query())
             if (result != 0):
-                raise HTTPException(status_code=401, detail=f"사용자 ID 중복! (id={user_model.id}, result={result})")
+                msg = f"회원가입 실패! (사용자 ID 중복 : id={user_model.id}, result={result})"
+                logger.error(msg)
+                raise HTTPException(status_code=400, detail=msg)
             if (user_model.check_validation() == False):
-                raise HTTPException(status_code=401, detail="유효하지 않은 데이터!")
+                msg = f"회원가입 실패! (유효하지 않은 데이터 : {user_model})"
+                logger.error(msg)
+                raise HTTPException(status_code=400, detail=msg)
             result = DB.EXECUTE(cursor, user_model.insert_query())
             if (result != 1):
-                raise HTTPException(status_code=401, detail=f"회원정보 추가 실패! (id={user_model.id}, result={result})")
+                msg = f"회원가입 실패! (DB 등록 실패 : id={user_model.id}, result={result})"
+                logger.error(msg)
+                raise HTTPException(status_code=500, detail=msg)
             connection.commit()
-            result = DB.EXECUTE(cursor, user_model.select_query())
-            if (result != 1):
-                raise HTTPException(status_code=401, detail=f"회원정보 찾기 실패! (id={user_model.id}, result={result})")
-            rows_tuple = cursor.fetchall()
-            user_model = UserModel(row=rows_tuple[0])
-            return user_model.model_dump()
+            user_model.id = cursor.lastrowid
+    logger.info(f"회원가입 성공 ({user_model})")
+    return user_model.model_dump()
 
 @router.post("/login", summary="로그인", response_model=LoginResponseModel)
-def login(user: LoginRequestModel):
+def login(request_model: LoginRequestModel):
     """
     아이디와 비밀번호를 받아 유효한 경우 JWT 토큰을 반환
     - **id**: 필수 입력
     - **pw**: 필수 입력
     """
+    logger.info(f"로그인 요청 ({request_model.id})")
+    logger.debug(f"Request ({request_model})")
     user_model = None
     name = os.getenv("DBNAME")
-    logger.debug(f"Request Body ({user})")
     with DB.CONNECT(name) as connection:
         with connection.cursor() as cursor:
-            result = DB.EXECUTE(cursor, user.check_query())
+            result = DB.EXECUTE(cursor, request_model.check_query())
             if (result == 0):
-                raise HTTPException(status_code=401, detail="아이디 또는 비밀번호 불일치!")
+                msg = f"로그인 실패! (아이디 또는 비밀번호 불일치 : {request_model.id})"
+                logger.error(msg)
+                raise HTTPException(status_code=400, detail=msg)
             rows_tuple = cursor.fetchall()
             user_model = UserModel(row=rows_tuple[0])
             logger.debug(f"UserModel({user_model})")
@@ -59,10 +72,11 @@ def login(user: LoginRequestModel):
         access_token = token,
         token_type = "bearer"
     )
+    logger.info(f"로그인 성공 ({user_model.id} {user_model.name})")
     return response.model_dump()
 
 @router.get("/test", summary="사용자 인증 확인")
 def test(auth: HTTPAuthorizationCredentials = Depends(security)):
     user_model = AuthJWT.get_user_model(auth)
     logger.debug(f"UserModel({user_model})")
-    return {"message": f"환영합니다! ({user_model})"}
+    return {"message": f"{user_model.name}님이 인증되었습니다! ({user_model.id})"}
--- routes/location.py
+++ routes/location.py
@@ -35,22 +35,24 @@ def search_keyword_kakaomap(request: Request, search_param: KakaoMapSearchReques
 @router.post("/register", summary="장소 등록")
 def register_location(location_model: LocationModel):
     """
-    사용자가 요청하는 장소를 추가
+    사용자가 요청하는 장소를 등록 (장소 찾기로 전달한 정보 그대로 사용)
     """
     dt = datetime.now()
-    logger.info(f"요청 수신 ({location_model})")
+    logger.info(f"장소 등록 요청 ({location_model.id} {location_model.name})")
     with DB.CONNECT() as connection:
         with connection.cursor() as cursor:
             result = DB.EXECUTE(cursor, LocationModel.SELECT_ID_QUERY(location_model.id))
             if (result != 0):
-                logger.error(f"처리 실패! (이미 등록된 장소)")
-                raise HTTPException(status_code=500, detail="장소 등록 실패")
+                msg = f"장소 등록 실패! (이미 등록된 장소 : {location_model.id} {location_model.name})"
+                logger.error(msg)
+                raise HTTPException(status_code=400, detail=msg)
             result = DB.EXECUTE(cursor, location_model.insert_query())
             connection.commit()
             if (result != 1):
-                logger.error(f"처리 실패! (장소 등록 실패)")
-                raise HTTPException(status_code=500, detail="장소 등록 실패")
+                msg = f"장소 등록 실패! (DB 등록 실패 : {location_model.id} {location_model.name})"
+                logger.error(msg)
+                raise HTTPException(status_code=500, detail=msg)
             location_model.pk = cursor.lastrowid
-    logger.info(f"처리 완료 ({location_model.pk}:{location_model.name}, {LOG.TO_ESTIMATED_TIME(dt)})")
+    logger.info(f"장소 등록 완료 ({location_model.pk}:{location_model.name}, {LOG.TO_ESTIMATED_TIME(dt)})")
     return {"message": f"등록 완료 ({location_model.pk}:{location_model.name})"}
  
