2a8720f6f1769ff41f82634414755d62800b4ce3
fss1208 <fss1208@naver.com>
2026-03-06 11:05:20 +0900

    20260305g.정리

--- main.py
+++ main.py
@@ -48,10 +48,10 @@ app.add_middleware(
 # tags: '/docs' & '/redoc' 페이지에서 해당 그룹으로 묶어서 표시
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 app.include_router(location.router, prefix="/location", tags=["Location Management"])
-app.include_router(favorite.router, prefix="/favorite", tags=["Favorite Management"])
-app.include_router(favorite_image.router, prefix="/favorite/image", tags=["Favorite Image Management"])
-app.include_router(favorite_location.router, prefix="/favorite/location", tags=["Favorite Location Management"])
 app.include_router(location_review.router, prefix="/location/review", tags=["Location Review Management"])
+app.include_router(favorite.router, prefix="/favorite", tags=["Favorite Management"])
+app.include_router(favorite_location.router, prefix="/favorite/location", tags=["Favorite Location Management"])
+app.include_router(favorite_image.router, prefix="/favorite/image", tags=["Favorite Image Management"])
 app.include_router(schedule.router, prefix="/schedule", tags=["Schedule Management"])
 app.include_router(schedule_location.router, prefix="/schedule/location", tags=["Schedule Location Management"])
 app.include_router(schedule_expense.router, prefix="/schedule/expense", tags=["Schedule Expense Management"])
